246b0fcc8b72a4f89ab14aa547d79eaf1ea6b129
jiruiyao <jiruiyao@git.cczet.com>
2017-07-14 17:03:32 +0800

    后台：增加查岗信息搜索功能

--- grails-app/controllers/com/hfyz/support/OwnerCheckRecordController.groovy
+++ grails-app/controllers/com/hfyz/support/OwnerCheckRecordController.groovy
@@ -1,26 +1,15 @@
 package com.hfyz.support
 
 import com.commons.utils.ControllerHelper
+import grails.converters.JSON
 
 class OwnerCheckRecordController implements ControllerHelper{
-
+    def ownerCheckRecordService
     def index() { }
 
     def list() {
-        def total = OwnerCheckRecord.count()
-        def checkRecordList = OwnerCheckRecord.list( [max: params.max, offset: params.offset])?.collect{recordObj->
-            [id: recordObj.id
-             ,auto: recordObj.auto
-             ,companyCode: recordObj.companyCode
-             ,dateCreated: recordObj.dateCreated.format("yyyy-MM-dd HH:mm:ss")
-             ,question: recordObj.question
-             ,answer: recordObj.answer
-             ,responsed: recordObj.responsed
-             ,operator: recordObj.operator?.name
-             ,responseDate: recordObj.responseDate ? recordObj.responseDate.format("yyyy-MM-dd HH:mm:ss") : null
-             ,responseContent: recordObj.responseContent
-             ,responseTime: recordObj.responseTime]
-        }
-        renderSuccessesWithMap([checkRecordList: checkRecordList, total: total])
+        def  checkResult= ownerCheckRecordService.list(params.max, params.offset, params.company,
+                                            params.startDate, params.endDate)
+        renderSuccessesWithMap([checkResult: checkResult])
     }
 }
--- grails-app/services/com/hfyz/support/OwnerCheckRecordService.groovy
+++ grails-app/services/com/hfyz/support/OwnerCheckRecordService.groovy
@@ -5,7 +5,83 @@ import grails.transaction.Transactional
 @Transactional
 class OwnerCheckRecordService {
 
-    def serviceMethod() {
+    def list(def max, def offset, def company, def startDate, def endDate) {
 
+        def result = [:]
+        def sd
+        def ed
+        if(dateConversion(startDate, endDate)) {
+             sd = dateConversion(startDate, endDate).sd
+             ed = dateConversion(startDate, endDate).ed
+        }
+        def total = OwnerCheckRecord.createCriteria().get {
+            projections {
+                count()
+            }
+            like ("companyCode", "${company}%")
+            if (!sd && ed) {
+                or {
+                    le("responseDate", ed)
+                }
+            }
+            if (sd && ed) {
+                or {
+                    between('responseDate', sd, ed)
+                }
+            }
+        }
+        def checkRecordList = OwnerCheckRecord.createCriteria().list([max:max, offset:offset]){
+            like ("companyCode", "${company}%")
+            if (!sd && ed) {
+                or {
+                    le("responseDate", ed)
+                }
+            }
+            if (sd && ed) {
+                or {
+                    between('responseDate', sd, ed)
+                }
+            }
+        }?.collect(){ OwnerCheckRecord recordObj ->
+            [id: recordObj.id
+             ,auto: recordObj.auto
+             ,companyCode: recordObj.companyCode
+             ,dateCreated: recordObj.dateCreated.format("yyyy-MM-dd HH:mm:ss")
+             ,question: recordObj.question
+             ,answer: recordObj.answer
+             ,responsed: recordObj.responsed
+             ,operator: recordObj.operator?.name
+             ,responseDate: recordObj.responseDate ? recordObj.responseDate.format("yyyy-MM-dd HH:mm:ss") : null
+             ,responseContent: recordObj.responseContent
+             ,responseTime: recordObj.responseTime ]
+        }
+
+        result << [checkRecordList: checkRecordList]
+        result << [total: total]
+        return result
+    }
+
+    def dateConversion (def startDate, def endDate) {
+        def date = [:]
+        Date sd
+        Date ed
+        if (startDate !='null' && endDate !='null') {
+            sd = new Date().parse('yyyy-MM-dd', startDate)
+            ed = new Date().parse('yyyy-MM-dd', endDate)
+            date << [sd: sd, ed: ed]
+            return date
+        }
+        if (startDate =='null' && endDate !='null') {
+            sd =  null
+            ed = new Date().parse('yyyy-MM-dd', endDate)
+            date << [sd: sd, ed: ed]
+            return date
+        }
+        if (startDate !='null' && endDate =='null') {
+            sd = new Date().parse('yyyy-MM-dd', startDate)
+            ed = new Date()
+            date << [sd: sd, ed: ed]
+            return date
+        }
     }
 }
