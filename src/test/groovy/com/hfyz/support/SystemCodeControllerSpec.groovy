38974e9f4cebc73490f921655a71ae84f0d0b60f
e <duweiwei@9ren.co>
2017-07-17 15:51:07 +0800

    SystemCodeController的测试

--- src/test/groovy/com/hfyz/support/SystemCodeControllerSpec.groovy
+++ src/test/groovy/com/hfyz/support/SystemCodeControllerSpec.groovy
@@ -21,7 +21,7 @@ class SystemCodeControllerSpec extends Specification {
 
     @Unroll
 
-    def "index:验证SystemCodeType为空和不为空时返回不同结果 "() {
+    def "index:根据SystemCodeType的状态返回相应结果 "() {
         setup:
         controller.supportService = [getSystemCodeListByParent: { parentId, type ->
             ['id': 1, 'codeNum': 2, 'name': 'aa', 'type': '1', 'parentId': null]
@@ -122,14 +122,27 @@ class SystemCodeControllerSpec extends Specification {
         type << [null, '']
     }
 
-    def "save: 验证保存，输入参数合理，在父类存在的情况下，保存子类，返回提示信息"() {
+    def "save: 验证保存，输入参数合理，在父类存在的情况下，保存子类，返回成功信息"() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-        def s1=LicenseType.build(name:'aa',codeNum: 'aa')
+        def licenseType1=LicenseType.build(name:'aa',codeNum: 'aa')
         when:
         params.type = 'LICENSE_TYPE'
         request.method = 'POST'
-        request.JSON = [id: '2', codeNum: 'aa', name: 'aa', type: 'LICENSE_TYPE', parentId:s1.id]
+        request.JSON = [id: '2', codeNum: 'aa', name: 'aa', type: 'LICENSE_TYPE', parentId:licenseType1.id]
+        controller.save()
+        then:
+        response.status == 200
+        response.json.result == 'success'
+
+    }
+    def "save: 验证保存，输入参数合理，保存子类，返回成功信息"() {
+        setup:
+        SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
+        when:
+        params.type = 'LICENSE_TYPE'
+        request.method = 'POST'
+        request.JSON = [id: '2', codeNum: 'aa', name: 'aa', type: 'LICENSE_TYPE']
         controller.save()
         then:
         response.status == 200
@@ -150,10 +163,9 @@ class SystemCodeControllerSpec extends Specification {
 
     }
 
-    def "edit:验证数据字典保存，传入type,id，当type,id为空或者为空字符串时：返回提示信息"() {
+    def "edit:验证数据字典保存，传入合理参数，当type为空或者为空字符串时：返回提示信息"() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-//        def s1 = LicenseType.build(codeNum: 'aa', name: 'aa')
         when:
         params.type = type
         params.id = id
@@ -165,6 +177,20 @@ class SystemCodeControllerSpec extends Specification {
         id   | type           | status | errors
         null | ''             | 400    | ['请求参数不合法，请查证！']
         null | null           | 400    | ['请求参数不合法，请查证！']
+
+    }
+    def "edit:验证数据字典保存，传入合理参数，当id为空或者为空字符串时：返回提示信息"() {
+        setup:
+        SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
+        when:
+        params.type = type
+        params.id = id
+        controller.edit()
+        then:
+        response.status == status
+        response.json.errors == errors
+        where:
+        id   | type           | status | errors
         null | 'LICENSE_TYPE' | 404    | ['找不到您请求的数据，请查正！']
         ''   | 'LICENSE_TYPE' | 404    | ['找不到您请求的数据，请查正！']
 
@@ -173,30 +199,30 @@ class SystemCodeControllerSpec extends Specification {
     def "edit:验证编辑，输入参数正确,编辑已存在的父类 返回编辑成功提示 "() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-        def s1 = LicenseType.build(codeNum: 'aaa', name: 'aa')
+        def licenseType1 = LicenseType.build(codeNum: 'aaa', name: 'aa')
         when:
         params.type = 'LICENSE_TYPE'
-        params.id = s1.id
+        params.id =licenseType1.id
         controller.edit()
         then:
         response.status == 200
-        response.json.systemCode == [name: 'aa', codeNum: 'aaa', id: s1.id]
+        response.json.systemCode == [name: 'aa', codeNum: 'aaa', id: licenseType1.id]
         response.json.result == 'success'
     }
 
     def "edit:  参数正确,编辑一个子类 返回编辑成功提示 "() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-        def s1 = LicenseType.build(codeNum: 'aa', name: 'aa')
-        def s2 = LicenseType.build(codeNum: 'bb', name: 'bb', parent: s1)
+        def licenseType1 = LicenseType.build(codeNum: 'aa', name: 'aa')
+        def licenseType2 = LicenseType.build(codeNum: 'bb', name: 'bb', parent:licenseType1)
         when:
         params.type = 'LICENSE_TYPE'
-        params.id = s2.id
+        params.id = licenseType2.id
         controller.edit()
         then:
         response.status == 200
-        response.json.parent == [codeNum: 'aa', name: 'aa', id: s1.id]
-        response.json.systemCode == [name: 'bb', codeNum: 'bb', id: s2.id]
+        response.json.parent == [codeNum: 'aa', name: 'aa', id: licenseType1.id]
+        response.json.systemCode == [name: 'bb', codeNum: 'bb', id: licenseType2.id]
         response.json.result == 'success'
     }
 
@@ -214,9 +240,9 @@ class SystemCodeControllerSpec extends Specification {
     def "delete:验证删除，输入正确的参数id,type，返回提示信息"() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-        def s1 = LicenseType.build(codeNum: 'aaa', name: 'aa')
+        def licenseType1 = LicenseType.build(codeNum: 'aaa', name: 'aa')
         when:
-        params.id = s1.id
+        params.id = licenseType1.id
         params.type = 'LICENSE_TYPE'
         controller.delete()
         then:
@@ -252,12 +278,12 @@ class SystemCodeControllerSpec extends Specification {
     def "update:验证更新，参数正确，返回正确消息 "() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-        def s1 = LicenseType.build(codeNum: 'aa', name: 'aa')
-        def s2 = LicenseType.build(codeNum: 'bb', name: 'bb')
+        def licenseType1 = LicenseType.build(codeNum: 'aa', name: 'aa')
+        def licenseType2 = LicenseType.build(codeNum: 'bb', name: 'bb')
         when:
         params.type = 'LICENSE_TYPE'
-        params.id = s2.id
-        request.JSON = [name: 'cc', codeNum: 'cc', type: 'LICENSE_TYPE', parentId: s1.id]
+        params.id = licenseType2.id
+        request.JSON = [name: 'cc', codeNum: 'cc', type: 'LICENSE_TYPE', parentId:licenseType1.id]
         controller.update()
         then:
         response.status == 200
@@ -267,11 +293,11 @@ class SystemCodeControllerSpec extends Specification {
     def "update:验证更新，如果parentId和子类自己的Id相等,返回错误消息 "() {
         setup:
         SystemCodeType.instance.types = [LICENSE_TYPE: [name: 'LicenseType', clazz: LicenseType, type: 'LICENSE_TYPE']]
-        def s1 = LicenseType.build(codeNum: 'aa', name: 'aa')
+        def licenseType1 = LicenseType.build(codeNum: 'aa', name: 'aa')
         when:
         params.type = 'LICENSE_TYPE'
-        params.id = s1.id
-        request.JSON = [name: 'bb', codeNum: 'bb', type: 'LICENSE_TYPE', parentId: s1.id]
+        params.id = licenseType1.id
+        request.JSON = [name: 'bb', codeNum: 'bb', type: 'LICENSE_TYPE', parentId: licenseType1.id]
         controller.update()
         then:
         response.status == 400
@@ -281,13 +307,13 @@ class SystemCodeControllerSpec extends Specification {
     def "getMenu:获取所有的菜单，返回正确的消息"() {
         setup:
         controller.supportService=[getMenu: {a->
-            [s:'s']}]
+            [menu:'menu']}]
         when:
         params.id=1
         controller.getmenu()
         then:
         response.status==200
-        response.json.s=='s'
+        response.json.menu=='menu'
         response.json.result=='success'
 
     }
