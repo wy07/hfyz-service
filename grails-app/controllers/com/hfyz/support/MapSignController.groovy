fe682c2a75a79bcbe71cb6f184032b37851a4212
jiruiyao <jiruiyao@git.cczet.com>
2017-07-19 12:01:47 +0800

    添加路标列表，删除路标，修改路标状态

--- grails-app/controllers/com/hfyz/support/MapSignController.groovy
+++ grails-app/controllers/com/hfyz/support/MapSignController.groovy
@@ -1,6 +1,44 @@
 package com.hfyz.support
 
-class MapSignController {
+import com.commons.utils.ControllerHelper
+
+class MapSignController implements ControllerHelper{
 
     def index() { }
+
+    def list(){
+        def mapSignList = MapSign.list([max:request.JSON.max, offset:request.JSON.offset, sort: "id"])?.collect { MapSign obj ->
+            [  id  : obj.id
+             , name: obj.name
+             , mapSignType: obj.mapSignType.name
+             , longitude: obj.longitude
+             , latitude: obj.latitude
+             , display: obj.display]
+        }
+        renderSuccessesWithMap([mapSignList: mapSignList, total: MapSign.count()])
+    }
+
+    def delete() {
+        withMapSign(params.long('id')) { mapSignInstance ->
+            mapSignInstance.delete(flush: true)
+            renderSuccess()
+        }
+    }
+
+    def changeDisplay() {
+        withMapSign(params.long('id')) { mapSignInstance ->
+            mapSignInstance.properties = request.JSON
+            mapSignInstance.save(flush: true, failOnError: true)
+            renderSuccess()
+        }
+    }
+    private withMapSign(Long id, Closure c) {
+        MapSign mapSignInstance = id ? MapSign.get(id) : null
+
+        if (mapSignInstance) {
+            c.call mapSignInstance
+        } else {
+            renderNoTFoundError()
+        }
+    }
 }
