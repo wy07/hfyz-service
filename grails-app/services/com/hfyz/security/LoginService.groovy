6aa108add665776ee0c676291d3de7a83cfa6227
e <duweiwei@9ren.co>
2017-07-20 17:08:20 +0800

    修改密码

--- grails-app/services/com/hfyz/security/LoginService.groovy
+++ grails-app/services/com/hfyz/security/LoginService.groovy
@@ -3,12 +3,17 @@ package com.hfyz.security
 import grails.transaction.Transactional
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import com.commons.exception.RecordNotFoundException
+import com.commons.exception.ParamsIllegalException
+import com.commons.utils.ValidationUtils
 
 
 @Transactional
 class LoginService {
 
     def authenticationManager
+    def springSecurityService
+
 
     def signIn(username, password) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password)
@@ -16,4 +21,27 @@ class LoginService {
         return authentication.getPrincipal()
     }
 
+    def getCurrentUserId(Long userId) {
+        User user = userId ? User.get(userId) : null
+        if (!user) {
+            throw new RecordNotFoundException()
+        }
+        user
+    }
+
+    def changePwd(User user, String originPwd, String newPwd) {
+        if (!user) {
+            throw new RecordNotFoundException()
+        }
+        String originPasswordHash = springSecurityService.encodePassword(originPwd, user.salt)
+        if (originPasswordHash != user.passwordHash) {
+            throw new ParamsIllegalException('旧密码错误')
+        }
+        if (!ValidationUtils.isStrongPassword(newPwd)) {
+            throw new ParamsIllegalException('密码必须由字母和数字组成,长度应在6位以上')
+        }
+        user.passwordHash = newPwd
+        user.salt = ValidationUtils.secureRandomSalt
+        user.save(flush: true, failOnError: true)
+    }
 }
