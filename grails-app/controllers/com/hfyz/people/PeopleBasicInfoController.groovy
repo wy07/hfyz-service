5e8a17bb7f7387f3ccfa71588db8c6500d410570
gaojl <jaylen.gao@hotmail.com>
2017-07-21 13:00:49 +0800

    修改dgate参数获取方式

--- grails-app/controllers/com/hfyz/people/PeopleBasicInfoController.groovy
+++ grails-app/controllers/com/hfyz/people/PeopleBasicInfoController.groovy
@@ -13,11 +13,9 @@ class PeopleBasicInfoController implements ControllerHelper {
      * @return
      */
     def list() {
-        println(params)
-        int max = PageUtils.getMax(params.max, 10, 100)
-        int offset = PageUtils.getOffset(params.offset)
-        def result = peopleBasicInfoService.getPeopleList(params.name, params.phoneNo, params.IDCardNo, max, offset)
-        println(result)
+        int max = PageUtils.getMax(request.JSON.max, 10, 100)
+        int offset = PageUtils.getOffset(request.JSON.offset)
+        def result = peopleBasicInfoService.getPeopleList(request.JSON.name, request.JSON.phoneNo, request.JSON.IDCardNo, max, offset)
         renderSuccessesWithMap([resultList: result.resultList, total: result.total])
     }
 
@@ -25,7 +23,7 @@ class PeopleBasicInfoController implements ControllerHelper {
      * 查看详情
      */
     def more() {
-        def result = peopleBasicInfoService.getDetailInfo(params.IDCardNo)
+        def result = peopleBasicInfoService.getDetailInfo(request.JSON.IDCardNo)
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         JSON.registerObjectMarshaller(java.sql.Timestamp) { o -> sdf.format(o) }
         renderSuccessesWithMap(result)
