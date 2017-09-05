69a6886e9c4ab05457abed30a93503f32f725c5e
jiruiyao <jiruiyao@git.cczet.com>
2017-09-05 13:40:13 +0800

    添加应急预案

--- grails-app/controllers/com/hfyz/waybill/EmergencyPlanController.groovy
+++ grails-app/controllers/com/hfyz/waybill/EmergencyPlanController.groovy
@@ -1,11 +1,18 @@
 package com.hfyz.waybill
 
 import com.commons.utils.ControllerHelper
+import com.commons.utils.PageUtils
 import com.hfyz.support.SystemCode
 
 class EmergencyPlanController implements ControllerHelper {
 
-    def index() { }
+    def emergencyPlanService
+
+    def list() {
+        int max = PageUtils.getMax(request.JSON.max, 10, 100)
+        int offset = PageUtils.getOffset(request.JSON.offset)
+        renderSuccessesWithMap(emergencyPlanService.getEmergencyPlanListAndTotal(max,offset))
+    }
 
     def getEmergencyPlanByDangerousType() {
         def dangerousType = SystemCode.get(request.JSON.id)
---
+++ grails-app/services/com/hfyz/waybill/EmergencyPlanService.groovy
@@ -0,0 +1,19 @@
+package com.hfyz.waybill
+
+import grails.transaction.Transactional
+
+@Transactional
+class EmergencyPlanService {
+
+    def getEmergencyPlanListAndTotal(def max, def offset) {
+        def emergencyPlanList = EmergencyPlan.list([max: max, offset: offset])?.collect { EmergencyPlan emergencyPlan->
+            [id       :emergencyPlan.id
+             ,name    :emergencyPlan.name
+             ,describe   :emergencyPlan.describe
+             ,dangerousType:emergencyPlan.dangerousType.name]
+        }
+
+        def total = EmergencyPlan.count()
+        return [emergencyPlanList: emergencyPlanList, total: total]
+    }
+}
